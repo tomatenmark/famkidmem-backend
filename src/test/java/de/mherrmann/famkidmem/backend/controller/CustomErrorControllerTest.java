2cbf547e905dd9950b3e90ca25981c03bcab76cd
tomatenmark <mh88@mailbox.org>
2020-05-02 13:15:27 +0200

    fixes error controller

--- src/test/java/de/mherrmann/famkidmem/backend/controller/CustomErrorControllerTest.java
+++ src/test/java/de/mherrmann/famkidmem/backend/controller/CustomErrorControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,14 +30,16 @@ public class CustomErrorControllerTest {
 
     @Test
     public void shouldGive405Response() throws Exception {
-        this.mockMvc.perform(post("/api/user/userKey/token"))
+        this.mockMvc.perform(get("/api/user/login"))
                 .andExpect(status().is(HttpStatus.METHOD_NOT_ALLOWED.value()));
     }
 
     @Test
     public void shouldGive415Response() throws Exception {
-        this.mockMvc.perform(get("/api/user/userKey/wrong")
+        this.mockMvc.perform(post("/api/user/login")
+                .contentType(MediaType.TEXT_PLAIN)
+                .content("test")
                 .contentType("test/html"))
-                .andExpect(status().is(HttpStatus.BAD_REQUEST.value()));
+                .andExpect(status().is(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()));
     }
 }
