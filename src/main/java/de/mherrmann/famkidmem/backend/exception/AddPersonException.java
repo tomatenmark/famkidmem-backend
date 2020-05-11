151b400619b6eeabf0eedf7e7c0547182f31e85b
tomatenmark <mh88@mailbox.org>
2020-05-11 23:04:25 +0200

    use only one exception for all actions

--- src/main/java/de/mherrmann/famkidmem/backend/exception/AddPersonException.java
+++
@@ -1,7 +0,0 @@
-package de.mherrmann.famkidmem.backend.exception;
-
-public class AddPersonException extends Exception {
-    public AddPersonException(String reason){
-        super("Could not add person. Reason: " + reason);
-    }
-}
