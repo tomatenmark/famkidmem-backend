6f109f7b3f6c45ec5d79f94e58de2265c28903e0
tomatenmark <mh88@mailbox.org>
2020-05-11 23:03:57 +0200

    refactor: use only one exception for all actions

--- src/main/java/de/mherrmann/famkidmem/backend/exception/UpdatePersonException.java
+++
@@ -1,7 +0,0 @@
-package de.mherrmann.famkidmem.backend.exception;
-
-public class UpdatePersonException extends Exception {
-    public UpdatePersonException(String reason){
-        super("Could not update person. Reason: " + reason);
-    }
-}
