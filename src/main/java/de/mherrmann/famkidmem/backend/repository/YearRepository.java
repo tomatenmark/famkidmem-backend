0bb9c644e29a83cb7ae03c8e1e596371e13c9554
tomatenmark <mh88@mailbox.org>
2020-05-16 15:27:39 +0200

    added yearEntityService

--- src/main/java/de/mherrmann/famkidmem/backend/repository/YearRepository.java
+++ src/main/java/de/mherrmann/famkidmem/backend/repository/YearRepository.java
@@ -4,7 +4,9 @@ import de.mherrmann.famkidmem.backend.entity.Year;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface YearRepository extends CrudRepository<Year, String> {
-
+    Optional<Year> findByValue(int value);
 }
