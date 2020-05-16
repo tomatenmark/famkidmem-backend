6c869a9c81364b3a13cb901468ad845566afdd9f
tomatenmark <mh88@mailbox.org>
2020-05-16 15:10:09 +0200

    add findByName

--- src/main/java/de/mherrmann/famkidmem/backend/repository/PersonRepository.java
+++ src/main/java/de/mherrmann/famkidmem/backend/repository/PersonRepository.java
@@ -4,7 +4,9 @@ import de.mherrmann.famkidmem.backend.entity.Person;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PersonRepository extends CrudRepository<Person, String> {
-
+    Optional<Person> findByName(String name);
 }
