88c5328b4d7a6495bf390d626e9fa9786699462e
tomatenmark <mh88@mailbox.org>
2020-05-26 17:39:12 +0200

    this was forgotten

--- src/main/java/de/mherrmann/famkidmem/backend/Application.java
+++ src/main/java/de/mherrmann/famkidmem/backend/Application.java
@@ -1,5 +1,6 @@
 package de.mherrmann.famkidmem.backend;
 
+import de.mherrmann.famkidmem.backend.utils.Bcrypt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,11 +10,12 @@ public class Application {
     public static String filesDir = "./files/";
 
     public static void main(String[] args) {
-
+        defineFilesDir(args);
+        Bcrypt.hash("preparing Brypt");
         SpringApplication.run(Application.class, args);
     }
 
-    private void defineFilesDir(String[] args){
+    private static void defineFilesDir(String[] args){
         for(String arg : args){
             if(arg.startsWith("--files_dir=")){
                 filesDir = arg.substring(arg.indexOf('=')+1);
