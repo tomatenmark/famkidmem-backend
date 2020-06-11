5975c21aeff518fd8116ccda23cb8abbc72efa4d
tomatenmark <mh88@mailbox.org>
2020-06-11 13:57:43 +0200

    file upload fix: file could not be uploaded if in tomcat environment

--- src/main/java/de/mherrmann/famkidmem/backend/service/ccms/FileUploadService.java
+++ src/main/java/de/mherrmann/famkidmem/backend/service/ccms/FileUploadService.java
@@ -9,6 +9,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 @Service
 public class FileUploadService {
@@ -36,8 +39,8 @@ public class FileUploadService {
         String fileName = multipartFile.getOriginalFilename().replaceAll("\\.+", ".");
         try {
             File destinationFile = new File(Application.filesDir + fileName);
-            destinationFile.createNewFile();
-            multipartFile.transferTo(destinationFile);
+            InputStream inputStream = multipartFile.getInputStream();
+            Files.copy(inputStream, destinationFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
         } catch(IOException ex){
             LOGGER.error("Could not save file {}. I/O Error", fileName, ex);
             throw new FileUploadException("Could not save file. I/O Error");
