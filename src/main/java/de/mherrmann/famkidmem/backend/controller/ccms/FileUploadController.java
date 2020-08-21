fa0a9dc31e181a7899b592d45c1ebb4246b81e42
tomatenmark <mh88@mailbox.org>
2020-08-21 22:10:18 +0200

    added config required since last dependency upgrade

--- src/main/java/de/mherrmann/famkidmem/backend/controller/ccms/FileUploadController.java
+++ src/main/java/de/mherrmann/famkidmem/backend/controller/ccms/FileUploadController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.annotation.MultipartConfig;
+
 @Controller
+@MultipartConfig
 public class FileUploadController {
 
     private final FileUploadService fileUploadService;
